fb2054265816cb1b30f55f45135622f3b2a5a2fb
Даниил Комаровских <komarovskih.d@yandex.ru>
2019-11-16 03:45:13 +0300

    Add possibility to insert records

--- src/database.cpp
+++ src/database.cpp
@@ -8,12 +8,17 @@ LibraryDataBase::LibraryDataBase() {
 void LibraryDataBase::initDB() {
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("library.db");
+    model = new QSqlTableModel;
+    query = new QSqlQuery;
+    maptables["autors"] = 0;
+    maptables["books"] = 1;
+    maptables["library_cards"] = 2;
+
 }
 
 void LibraryDataBase::createTables() {
-    QSqlQuery query;
-    query.exec("PRAGMA foreign_keys = ON;");
-    query.exec("CREATE TABLE IF NOT EXISTS books "
+    query->exec("PRAGMA foreign_keys = ON;");
+    err = query->exec("CREATE TABLE IF NOT EXISTS books "
                "(book_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, "
                "LBC_number VARCHAR(20), "
                "UDC_number VARCHAR(20), "
@@ -21,20 +26,23 @@ void LibraryDataBase::createTables() {
                "publication_place VARCHAR(35), "
                "title VARCHAR(40), "
                "amount INTEGER)");
-    qDebug() << query.lastError();
-    query.exec("CREATE TABLE IF NOT EXISTS authors "
+    if (!err)
+        qDebug() << query->lastError();
+    err = query->exec("CREATE TABLE IF NOT EXISTS authors "
                "(author_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, "
                "first_name VARCHAR(15), "
                "last_name VARCHAR(20))");
-    qDebug() << query.lastError();
-    query.exec("CREATE TABLE IF NOT EXISTS book_author "
+    if (!err)
+        qDebug() << query->lastError();
+    err = query->exec("CREATE TABLE IF NOT EXISTS book_author "
                "(book_id INTEGER, "
                "author_id INTEGER, "
                "FOREIGN KEY(book_id) REFERENCES books(book_id), "
                "FOREIGN KEY(author_id) REFERENCES authors(author_id), "
                "CONSTRAINT new_pk PRIMARY KEY (book_id, author_id))");
-    qDebug() << query.lastError();
-    query.exec("CREATE TABLE IF NOT EXISTS library_cards "
+    if (!err)
+        qDebug() << query->lastError();
+    err = query->exec("CREATE TABLE IF NOT EXISTS library_cards "
                "(card_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, "
                "passport_info VARCHAR(40), "
                "first_name VARCHAR(15), "
@@ -43,8 +51,9 @@ void LibraryDataBase::createTables() {
                "phone_number VARCHAR(15), "
                "address VARCHAR(40), "
                "photo VARBINARY)");
-    qDebug() << query.lastError();
-    query.exec("CREATE TABLE IF NOT EXISTS book_out "
+    if (!err)
+        qDebug() << query->lastError();
+    err = query->exec("CREATE TABLE IF NOT EXISTS book_out "
                "(book_id INTEGER, "
                "card_id INTEGER, "
                "delivery_date DATE, "
@@ -53,17 +62,49 @@ void LibraryDataBase::createTables() {
                "FOREIGN KEY(book_id) REFERENCES books(book_id), "
                "FOREIGN KEY(card_id) REFERENCES library_cards(card_id), "
                "CONSTRAINT new_pk PRIMARY KEY (book_id, card_id))");
-    qDebug() << query.lastError();
+    if (!err)
+        qDebug() << query->lastError();
 }
 
 void LibraryDataBase::showTable(QString table) {
     QTableView *view = new QTableView;
-    QSqlTableModel *model = new QSqlTableModel;
     model->setTable(table);
     model->select();
     model->setEditStrategy(QSqlTableModel::OnFieldChange);
     view->setModel(model);
     view->show();
+}
+// или я тупой, или в плюсах нет нормального форматирования строк и придется заниматься чем то странным
+// да и в принципе я не нашел нормального способа вставлять в бд записи, кроме как через sql запросы
+void LibraryDataBase::insertRecord(QString table, QMap<QString, QString> kwargs) {
+    switch(maptables[table]) {
+        case 0:
+            err = query->exec(QString("INSERT into %1(first_name, last_name)" // значение должно быть в одинарных кавычках если это строка иначе гг
+                                      "VALUES ('%2', '%3');").arg(table).arg(kwargs["first_name"])
+                                      .arg(kwargs["last_name"]));
+            if (!err)
+                qDebug() << query->lastError();
+            break;
+        case 1:
+            err = query->exec(QString("INSERT into %1(LBC_number, UDC_number, publication_year,"
+                                      " publication_place, title, amount)""VALUES ('%2', '%3', %4, '%5', '%6', %7);")
+                                      .arg(table).arg(kwargs["LBC_number"]).arg(kwargs["UDC_number"])
+                                      .arg(kwargs["publication_year"]).arg(kwargs["publication_place"])
+                                      .arg(kwargs["title"]).arg(kwargs["amount"]));
+            if (!err)
+                qDebug() << query->lastError();
+            break;
+        case 2:
+            err = query->exec(QString("INSERT into %1(passport_info, first_name, last_name,"
+                                      " card_type, phone_number, address, photo)""VALUES ('%2', '%3', '%4', '%5', '%6', '%7', '%8');")
+                                      .arg(table).arg(kwargs["passport_info"]).arg(kwargs["first_name"])
+                                      .arg(kwargs["last_name"]).arg(kwargs["card_type"])
+                                      .arg(kwargs["phone_number"]).arg(kwargs["address"]).arg(kwargs["photo"]));
+            if (!err)
+                qDebug() << query->lastError();
+            break;
+    default: break;
+    }
 }
 
 void LibraryDataBase::openDB() {
--- src/database.h
+++ src/database.h
@@ -7,11 +7,17 @@
 class LibraryDataBase {
 
 public:
+    QSqlTableModel *model;
+    QSqlQuery *query;
+    QMap <QString, int> maptables;
+    bool err;
+
     LibraryDataBase();
 
     void initDB();
     void createTables();
     void showTable(QString table);
+    void insertRecord(QString table, QMap<QString, QString> kwargs);
     void openDB();
     void closeDB();
 
